cb531e34c80493d5290337086ec2fe3c5acd43d6
Dayana Gutierrez <dayi.guti83@gmail.com>
2017-03-15 01:05:22 -0400

    Add files via upload

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'autos',
+    'version': '1.0',
+    'depends': ['base_setup'],
+    'autor': 'Dayana Gutierrez',
+    'category': '',
+    'description': """modelo de autos""",
+    'update_xml': [],
+    "data": [
+        "views/autos_views.xml",
+        "views/modelo_views.xml",
+      
+       ],
+    'installable': True,
+    'auto_install': False,
+}
